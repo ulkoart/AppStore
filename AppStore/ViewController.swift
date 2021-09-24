e372e55e73d554a9f8bcc570121828ba69b9abd6
ulkoart <ulkoart@gmail.com>
2021-09-24 20:04:35 +0300

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by user on 24.09.2021.
+//  Copyright © 2021 ulkoart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
