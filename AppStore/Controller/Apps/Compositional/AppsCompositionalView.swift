bcb38e259159f51a97358f45853ca587c35e6265
Artem Ulko <ulkoart@gmail.com>
2022-07-10 10:41:19 +0300

    Layout Groups Items Sections

--- AppStore/Controller/Apps/Compositional/AppsCompositionalView.swift
+++ AppStore/Controller/Apps/Compositional/AppsCompositionalView.swift
@@ -12,7 +12,19 @@ class CompositionalController: UICollectionViewController {
 
 	init() {
 		// UICollectionViewCompositionalLayout()
-		super.init(collectionViewLayout: UICollectionViewFlowLayout())
+
+		let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+		item.contentInsets.bottom = 16
+		item.contentInsets.trailing = 16
+
+		let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.75), heightDimension: .absolute(300)), subitems: [item])
+		let section = NSCollectionLayoutSection(group: group)
+		section.orthogonalScrollingBehavior = .groupPaging
+		section.contentInsets.leading = 16
+
+		let layout = UICollectionViewCompositionalLayout(section: section)
+
+		super.init(collectionViewLayout: layout)
 	}
 	
 	required init?(coder: NSCoder) {
@@ -20,7 +32,7 @@ class CompositionalController: UICollectionViewController {
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 150
+		return 5
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
